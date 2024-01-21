8ddb6ee5cdf7bc8d3b47a55ef33ceae28be39d8b
Random <155200367+lidth@users.noreply.github.com>
2024-01-21 13:42:08 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Frontend Mentor - FAQ accordion
+
+![Design preview for the FAQ accordion coding challenge](./design/desktop-preview.jpg)
+
+Built with
+
+Semantic HTML5 Markup
+
+CSS custom properties
+
+A little bit of JS
+
+Author:
+    https://github.com/lidth
---
+++ assets/script.js
@@ -0,0 +1,35 @@
+function iplus(n) {
+    let mae = document.getElementsByClassName('dmother')[n];
+    let icon = document.getElementsByClassName('plusicon')[n];
+    
+    if(mae.lastElementChild.style.display == 'none') {
+        mae.lastElementChild.style.display = 'block'
+        icon.src = 'assets/images/icon-minus.svg'
+    } else {
+        mae.lastElementChild.style.display = 'none'
+        icon.src = 'assets/images/icon-plus.svg'
+    }
+}
+
+
+
+
+
+/*function carregar() {
+
+    var subitem1 = document.getElementById('sub-item-1')
+    var icon = document.querySelector('img.plusicon')
+    if (subitem1.style.display == 'none') {
+        subitem1.style.display = 'block'
+        icon.src = 'assets/images/icon-minus.svg'
+    } else {
+        subitem1.style.display = 'none'
+        icon.src = 'assets/images/icon-plus.svg'
+    }
+}
+
+function iplus() {
+    carregar()
+}
+*/
+//I used the "carregar()" function because using just "iplus()" the first click was not activating the operation, note: starting now
